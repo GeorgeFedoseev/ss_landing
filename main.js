98068fc9959b84f3c9a63ea33bc27a5935a64aa8
George <spamgoga@gmail.com>
2014-10-19 18:43:49 +0400

    no message

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ $(document).ready(function() {
  
   var owl = $("#testimonials_crs");
   owl.owlCarousel({
-      items : 1
+      items : 1,
+      itemsMobile : false,
+      itemsDesktopSmall : false,      
+      itemsTablet: false,
+      singleItem:true
   });
 
-  $("#testimonials_next").click(function(){  	
+  $("#testimonials_next").click(function(){
+  	
     owl.trigger('owl.next');
   });
 
