452f80d4b8aa465640427737191036a84cc8ae50
George <spamgoga@gmail.com>
2014-10-20 19:00:21 +0400

    no message

--- main.js
+++ main.js
@@ -18,5 +18,21 @@ $(document).ready(function() {
   $("#testimonials_prev").click(function(){  	
     owl.trigger('owl.prev');
   });
+
+  $("input[type='submit']").click(function(eventObject){
+    var input = $(this).parent().find("input[type='email']");
+    var email = input.val();
+    if(validateEmail(email)){
+      alert(email);
+    }else{
+      // wrong email
+        $(input).parent().effect( "shake", {distance: 5} );
+    }
+  });
  
 });
+
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
